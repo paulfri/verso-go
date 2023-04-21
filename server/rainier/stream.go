f2a15be36cb48ebfdc4ab5ff88ec84ca49443d0c
Paul Friedman <paulfri@hey.com>
2023-04-21 16:24:06 -0700

    Add params to stream

--- server/rainier/stream.go
+++ server/rainier/stream.go
@@ -6,16 +6,40 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-// r: sort criteria. Items are sorted by date (descending by default), r=o inverts the order.
-// n: the number of items per page. Default: 20.
-// c: the continuation string (see below).
-// xt: a stream ID to exclude from the list.
-// it: a steam ID to include in the list.
-// ot: an epoch timestamp. Items older than this timestamp are filtered out.
-// nt: an epoch timestamp. Items newer than this timestamp are filtered out.
+type SortOrderValue string
+
+const (
+	Ascending  SortOrderValue = "o"
+	Descending SortOrderValue = "a"
+)
+
+type StreamContentsRequestParams struct {
+	// r: sort criteria. Items are sorted by date (descending by default), r=o inverts the order.
+	SortOrder SortOrderValue `query:"r"`
+	// n: the number of items per page. Default: 20.
+	NumItems int `query:"n"`
+	// c: the continuation string (see below).
+	Continuation string `query:"c"`
+	// xt: a stream ID to exclude from the list.
+	Exclude string `query:"xt"`
+	// it: a steam ID to include in the list.
+	Include string `query:"it"`
+	// ot: an epoch timestamp. Items older than this timestamp are filtered out.
+	ExcludeOlderThan int `query:"ot"`
+	// nt: an epoch timestamp. Items newer than this timestamp are filtered out.
+	ExcludeNewerThan int `query:"nt"`
+}
 
 func (c *RainierController) StreamContents(w http.ResponseWriter, req *http.Request) {
-	switch streamID := chi.URLParam(req, "*"); streamID {
+	params := StreamContentsRequestParams{}
+	err := c.Container.Params(&params, req)
+
+	if err != nil {
+		c.Container.Render.Text(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	switch streamId := chi.URLParam(req, "*"); streamId {
 	case "user/-/state/com.google/reading-list":
 		c.Container.Render.Text(w, http.StatusOK, "reading list")
 	default:
