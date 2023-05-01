c97a5322998b711e45dac1c58db856a9471f5503
Paul Friedman <paulfri@hey.com>
2023-05-01 16:31:36 -0700

    Add env vars

--- core/action/config.go
+++ core/action/config.go
@@ -9,16 +9,34 @@ import (
 
 func Config(config *config.Config) cli.ActionFunc {
 	return func(cliContext *cli.Context) error {
+		var sslMode string
+		if config.Database.SSLDisabled {
+			sslMode = "disable"
+		} else {
+			sslMode = "require"
+		}
+
 		switch cliContext.Args().Get(0) {
+		case "dbname":
+			fmt.Println(config.Database.Database)
 		case "database":
-			fmt.Printf("%s\n", config.Database.Database)
+			fmt.Printf(
+				"postgresql://%s:%s@%s:%d/%s?sslmode=%s",
+				config.Database.User,
+				config.Database.Password,
+				config.Database.Host,
+				config.Database.Port,
+				config.Database.Database,
+				sslMode,
+			)
 		case "goose":
 			fmt.Printf(
-				"host=%s user=%s password=%s dbname=%s sslmode=disable",
+				"host=%s user=%s password=%s dbname=%s sslmode=%s",
 				config.Database.Host,
 				config.Database.User,
 				config.Database.Password,
 				config.Database.Database,
+				sslMode,
 			)
 		default:
 			panic("Unknown argument")
