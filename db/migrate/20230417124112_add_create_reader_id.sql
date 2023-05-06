1b92dbeb4252a1acd2cccfbc0c1630a58bd57007
Paul Friedman <paulfri@hey.com>
2023-05-06 08:16:05 -0700

    Only set reader_id if empty (#16)
    This makes restoring seeds easier as the existing reader_id isn't
    overwritten (causing flaky snapshots).
    
    Best to reset the production database after merging since it's editing
    an old migration. Not a huge deal, probably, but keeps the schemas in
    sync.

--- db/migrate/20230417124112_add_create_reader_id.sql
+++ db/migrate/20230417124112_add_create_reader_id.sql
@@ -3,21 +3,25 @@
 create or replace function generate_reader_id()
 returns trigger as $$
 begin
-  -- reader_id is stored as a 16-length zero-added hex representation of a
-  -- 64-bit integer (bigint)
-  new.reader_id = lpad(
-    -- convert to hex
-    to_hex(
-      -- floor to convert the random numeric to whole number
-      floor(
-        -- 9223372036854775807 is the maximum value of a signed 64-bit integer
-        -- random() is cast to numeric for greater precision (otherwise it
-        -- reduces the number of possible random bigints)
-        random()::numeric * 9223372036854775807
-      -- cast to bigint for insertion
-      )::bigint),
-    16,
-    '0'
+  -- sets reader_id to a valid value if not set in the insert
+  new.reader_id = coalesce(
+    old.reader_id,
+    -- reader_id is stored as a 16-length zero-added hex representation of a
+    -- 64-bit integer (bigint)
+    lpad(
+      -- convert to hex
+      to_hex(
+        -- floor to convert the random numeric to whole number
+        floor(
+          -- 9223372036854775807 is the maximum value of a signed 64-bit integer
+          -- random() is cast to numeric for greater precision (otherwise it
+          -- reduces the number of possible random bigints)
+          random()::numeric * 9223372036854775807
+        -- cast to bigint for insertion
+        )::bigint),
+      16,
+      '0'
+    )
   );
 
   return new;
